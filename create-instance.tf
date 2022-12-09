335ff8e1097b9682eb01620053a11bff2e0fc669
Inchara C R <64958424+incharacr@users.noreply.github.com>
2022-12-09 14:22:27 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "NewInstance" {
-  ami           = "ami-0af2f764c580cc1f9"
+resource "aws_instance" "NewInstances" {
+  ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
   
   tags = {
